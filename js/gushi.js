6a1c85187d07a6586f20af93c9674d979dd5325e
Jermaine Forbes <jermaine0forbes@gmail.com>
2016-11-21 12:13:29 -0500

    Fixed window width issue

--- js/gushi.js
+++ js/gushi.js
@@ -1,32 +1,32 @@
 window.onload = function(){
 
-var menu = document.getElementsByClassName('menu-fixed')[0];
-var topHeader = document.getElementsByClassName('top-header')[0];
-var botHeader = document.getElementsByClassName('bottom-header')[0];
+	var menu = document.getElementsByClassName('menu-fixed')[0];
+	var topHeader = document.getElementsByClassName('top-header')[0];
+	var botHeader = document.getElementsByClassName('bottom-header')[0];
 
 
-window.addEventListener('scroll', function(){
-var width = window.innerWidth;
-var position = window.pageYOffset;
+	window.addEventListener('scroll', function(){
+		var width = window.innerWidth;
+		var position = window.pageYOffset;
 
-console.log();
+		console.log();
 
-if ( position > 217){
- topHeader.style.height = "67px";
-botHeader.style.height = "67px";
-  menu.style.visibility ="visible";
-}else{
-  topHeader.style.height = "0";
- botHeader.style.height = "0";
-menu.style.visibility ="hidden";
-}
+		if ( position > 602){
+			 topHeader.style.height = "67px";
+			botHeader.style.height = "67px";
+			  menu.style.visibility ="visible";
+		}else{
+			  topHeader.style.height = "0";
+			 botHeader.style.height = "0";
+			menu.style.visibility ="hidden";
+		}
 
-if(width < 1025){
-menu.style.height = "75px";
+		if(width < 1025){
+		menu.style.height = "75px";
 
-}
+		}
 
-})//addEventListener
+	})//addEventListener
 
 
 }//onload
